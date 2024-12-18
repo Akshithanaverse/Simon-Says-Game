ec9dab411efd674618c5fd7a7c168b445c0bff29
akshithanaverse <akshithanaverse24@gmail.com>
2024-12-18 22:34:07 +0530

    Modified script.js and style.css

---
+++ script.js
@@ -0,0 +1,34 @@
+let game_seq=[];
+let user_seq=[];
+let btns=["yellow","red","green","blue"];
+let started=false;
+let level=0;
+let h2=document.querySelector("h2");
+document.addEventListener("keypress",function()
+{
+    if(started==false)
+    {
+        console.log("game has been started");
+        started=true;
+        levelUp();
+    }
+});
+function btnFlash(btn)
+{
+    btn.classList.add("flash");
+    setTimeout(function()
+{
+    btn.classList.remove("flash")
+},1000);
+}
+function levelUp()
+{
+    level++;
+    h2.innerText=`Level ${level}`;
+
+    //random button choose
+    let randIndx=Math.floor(Math.random()*4);
+    let randColor=btns[randIndx];
+    let randBtn=document.querySelector(`.${randColor}`);
+    btnFlash(randBtn);
+}
